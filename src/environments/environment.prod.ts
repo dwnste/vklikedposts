69d991cb9bab3a679ceea0953267652824a1ed13
dwnste <dwnste@gmail.com>
2017-07-07 14:31:47 +0300

    .

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  get_token_url: 'https://oauth.vk.com/authorize?client_id=6099251&scope=8192&redirect_uri=https://dwnste.github.io/vklikedposts/auth&response_type=token'
-};
