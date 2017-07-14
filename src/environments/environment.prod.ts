8683104785ecebc478971c748c340ce981c98549
dwnste <dwnste@gmail.com>
2017-07-14 11:55:43 +0300

    .

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-const CLIENT_ID = 6099251, // your vk app id
-      SCOPE = 8192, // permissions to be able to work with wall api methods. more:  https://vk.com/dev/permissions
-      REDIRECT_URL = 'https://dwnste.github.io/vklikedposts/auth';
-export const environment = {
-  production: true,
-  get_token_url: `https://oauth.vk.com/authorize?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT_URL}&response_type=token`
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-const
-  CLIENT_ID = 6099251, // your vk app id
-  SCOPE = 8192, // permissions to be able to work with wall api methods. more:  https://vk.com/dev/permissions
-  REDIRECT_URL = 'http://localhost:4200/auth';
-
-export const environment = {
-  production: false,
-  get_token_url: `https://oauth.vk.com/authorize?client_id=${CLIENT_ID}&scope=${SCOPE}&redirect_uri=${REDIRECT_URL}&response_type=token`
-};
