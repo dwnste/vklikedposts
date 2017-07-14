7a60ea7067291587439153d395547a0e50e595cf
dwnste <dwnste@gmail.com>
2017-07-14 17:55:20 +0300

    удалил лишние куски кода

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,21 +2,17 @@ import * as fetchJsonp from 'fetch-jsonp';
 import * as moment from 'moment';
 import * as async from 'async';
 import * as VK from 'vk-openapi';
-import { Component, OnInit, Injectable } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 
 moment.locale('ru');
 
 VK.init({apiId: 6099251});
 
 @Injectable()
-export class AppService implements OnInit {
-    myVK = null;
+export class AppService {
     state = {
         isGettingPosts: false,
-        isCheckingLikes: false
     }
-
-    ngOnInit() {}
 
     getApi() {
         return VK;
@@ -107,27 +103,6 @@ export class AppService implements OnInit {
                 .catch( ex => console.log('parsing failed', ex) );
     };
 
-    isLiked({user_id, type = 'post', owner_id, item_id, user_access_token}) {
-        const url = `
-        https://api.vk.com/api.php?
-            oauth=1&
-            method=likes.isLiked&v=5.65&
-            user_id=${user_id}&
-            type=${type}&
-            owner_id=${owner_id}&
-            item_id=${item_id}&
-            access_token=${user_access_token}
-            `.replace(/ /g, '')
-             .replace(/\n/g, '');
-
-        return fetchJsonp(url)
-                .then( response => response.json())
-                .then( ({ response }) => {
-                            return response;
-                        })
-                .catch( ex => console.log('parsing failed', ex) );
-    };
-
     formatPost(post, response) {
       post.text = post.text.replace(/<br\s*\/?>/gi, ' ');
       post.date = moment(post.date * 1000).format('LL');
@@ -175,7 +150,6 @@ export class AppService implements OnInit {
     };
 
     getAllLikedPosts({owner_id, user_id, user_access_token, posts}) {
-        this.state.isCheckingLikes = true;
         const bundle = this.bundleQuery('likes.isLiked', posts, user_id, owner_id);
         return this.execute({user_access_token, code: bundle})
             .then((likes: any) => {
