ace2f8638bd74726469b0f34b528d798658d73c9
dwnste <dwnste@gmail.com>
2017-07-17 16:26:50 +0300

    пофиксил "показать оригинал"

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -61,7 +61,7 @@ export class FeedComponent implements OnInit {
 
     showOriginal(post) {
       if (post.post_type === 'post') {
-        window.open(`https://vk.com/wall${post.to_id}_${post.id}`, '_blank');
+        window.open(`https://vk.com/wall${post.from_id}_${post.id}`, '_blank');
       }
     }
 
