283300e4628b432c7e7c355cf95d89d0e4e34f45
dwnste <dwnste@gmail.com>
2017-07-20 15:05:02 +0300

    fix post type

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  @Input() post: Array<any>;
+  @Input() post: any;
 
   constructor() { }
 
